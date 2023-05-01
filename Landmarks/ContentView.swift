a3252ec693de7ad8bda5c8f4ade92a1d77b26657
Jo Lynn <jojo.lynn@apple.com>
2023-05-01 13:56:06 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jo Lynn on 2023-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jo Lynn on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
